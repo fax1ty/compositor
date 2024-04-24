6c4531c2089b5cac6db03ac1b2bc7a32749da1d3
Davydov Artemiy <artemiy.davydov@mercurydevelopment.com>
2024-04-24 20:50:35 +0300

    feature: supabase as database provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { signIn, signOut, auth, handlers } = NextAuth({
   providers: [GitHub],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }),
 });
